8a40412289c006bb5d5be4b96d01ad9a6e696cb4
Asynchronous-Javascripter <asynchronous.devin@gmail.com>
2024-07-20 17:01:11 +0530

    fixed the bug, of Cannon Js importing, [as cannon was not imported by default]

--- browser/revolution/Browser_Packages.js
+++ browser/revolution/Browser_Packages.js
@@ -1,8 +1,10 @@
 import * as THREE from "three";
-import * as CANNON from "cannon";
+import * as cannon from "cannon";
 import millify from "millify";
 import cannonDebugger from "cannon-es-debugger";
 import gsap from "gsap";
+
+const CANNON = cannon.default;
 
 export {
   THREE,
